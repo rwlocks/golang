8d09fa481bda938b04515746336afe228970796d
rwlocks <7119168@qq.com>
2022-04-24 16:07:45 +0800

    delete useless code

--- brocast/client.go
+++ brocast/client.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
 func main() {
-	$END$
+
+	conn, err := net.Dial("tcp", "127.0.0.1:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	go recvMessage(conn)
+	sendMessage(conn)
+}
+
+func recvMessage(conn net.Conn) {
+	scan := bufio.NewScanner(conn)
+	for scan.Scan() {
+
+		fmt.Println(scan.Text())
+	}
+}
+
+func sendMessage(conn net.Conn) {
+	input := bufio.NewScanner(os.Stdin)
+	fmt.Println("start to scan")
+	for input.Scan() {
+		str := input.Text() + "\n"
+		io.WriteString(conn, str)
+	}
 }
--- brocast/server.go
+++ brocast/server.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"bufio"
+	"io"
+	"log"
+	"net"
+	"strings"
+)
+
 func main() {
-	$END$
+	s, err := net.Listen("tcp", "127.0.0.1:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := s.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	scan := bufio.NewScanner(conn)
+	for scan.Scan() {
+		upper := strings.ToUpper(scan.Text()) + "\n"
+		io.WriteString(conn, upper)
+	}
 }
